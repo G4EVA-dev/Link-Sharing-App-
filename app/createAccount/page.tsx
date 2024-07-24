9e224483f2d559ebbf3521c43bb58a1d1ae23d73
G4EVA-dev <tanzeglenn@gmail.com>
2024-07-24 23:50:44 +0100

    set route for login and createAccount

--- app/createAccount/page.tsx
+++ app/createAccount/page.tsx
@@ -1,5 +1,6 @@
-"use client"
+"use client";
 import React, { useState, FormEvent, ChangeEvent } from "react";
+import { useRouter } from "next/navigation";
 import Image from "next/image";
 import Link from "next/link";
 import { auth } from "@/firebase"; // Adjust the import based on your file structure
@@ -10,6 +11,7 @@ function CreateAccount() {
   const [password, setPassword] = useState<string>("");
   const [confirmPassword, setConfirmPassword] = useState<string>("");
   const [error, setError] = useState<string>("");
+  const router = useRouter();
 
   const handleSignUp = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -19,10 +21,16 @@ function CreateAccount() {
     }
 
     try {
-      const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+      const userCredential = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
       console.log(userCredential);
       // Redirect to login page or dashboard after successful sign up
-    } catch (error: any) { // Use `any` to access error properties
+      router.push("/login");
+    } catch (error: any) {
+      // Use `any` to access error properties
       console.error(error);
       setError(error.message);
     }
@@ -53,7 +61,10 @@ function CreateAccount() {
             Let’s get you started sharing your links!
           </p>
         </div>
-        <form onSubmit={handleSignUp} className="flex flex-col height-auto w-full">
+        <form
+          onSubmit={handleSignUp}
+          className="flex flex-col height-auto w-full"
+        >
           <label
             htmlFor="email"
             className="font-normal text-[12px] leading-[18px] text-darkGray mb-[4px]"
@@ -73,7 +84,9 @@ function CreateAccount() {
                 backgroundSize: "16px",
               }}
               value={email}
-              onChange={(e: ChangeEvent<HTMLInputElement>) => setEmail(e.target.value)}
+              onChange={(e: ChangeEvent<HTMLInputElement>) =>
+                setEmail(e.target.value)
+              }
             />
           </div>
           <label
@@ -95,7 +108,9 @@ function CreateAccount() {
                 backgroundSize: "16px",
               }}
               value={password}
-              onChange={(e: ChangeEvent<HTMLInputElement>) => setPassword(e.target.value)}
+              onChange={(e: ChangeEvent<HTMLInputElement>) =>
+                setPassword(e.target.value)
+              }
             />
           </div>
           <label
@@ -117,7 +132,9 @@ function CreateAccount() {
                 backgroundSize: "16px",
               }}
               value={confirmPassword}
-              onChange={(e: ChangeEvent<HTMLInputElement>) => setConfirmPassword(e.target.value)}
+              onChange={(e: ChangeEvent<HTMLInputElement>) =>
+                setConfirmPassword(e.target.value)
+              }
             />
           </div>
           {error && <p className="text-red-500">{error}</p>}
@@ -145,124 +162,3 @@ function CreateAccount() {
 }
 
 export default CreateAccount;
-
-
-
-
-
-// import React from "react";
-// import Image from "next/image";
-// import Link from "next/link";
-
-// function CreateAccount() {
-//   return (
-//     <div className="p-[32px] md:bg-bgColor flex flex-col items-start justify-center md:items-center h-screen ">
-//       <div className="flex gap-[10px] mb-[64px] md:mb-[51px] ">
-//         <Image
-//           src="/images/Vector.svg"
-//           alt="Image vector"
-//           width="34"
-//           height="34"
-//         />
-//         <Image
-//           src="/images/devlinks.svg"
-//           alt="devlinks"
-//           width="136"
-//           height="27"
-//         />
-//       </div>
-//       <div className="w-[311px]  h-auto flex flex-col  md:bg-loginBg md:w-[476px] md:p-[40px] ">
-//         <div className="mb-[40px]">
-//           <h1 className="font-bold text-[24px] leading-[36px] mb-[8px]">
-//             Create Account
-//           </h1>
-//           <p className="font-normal text-[16px] leading-[24px] text-darkGray">
-//             Let’s get you started sharing your links!
-//           </p>
-//         </div>
-//         <form action="" className="flex flex-col height-auto w-full">
-//           <label
-//             htmlFor="email"
-//             className="font-normal text-[12px] leading-[18px] text-darkGray mb-[4px]"
-//           >
-//             Email address
-//           </label>
-//           <div className="relative mb-[24px]">
-//             <input
-//               type="email"
-//               id="email"
-//               placeholder="e.g. alex@email.com"
-//               className="pl-[44px] pr-[12px] py-[10px] border border-black rounded-md w-full"
-//               style={{
-//                 backgroundImage: "url(/images/email-icon.svg)",
-//                 backgroundRepeat: "no-repeat",
-//                 backgroundPosition: "10px center",
-//                 backgroundSize: "16px",
-//               }}
-//             />
-//           </div>
-//           <label
-//             htmlFor="password"
-//             className="font-normal text-[12px] leading-[18px] text-darkGray mb-[4px]"
-//           >
-//             Password
-//           </label>
-//           <div className="relative mb-[24px]">
-//             <input
-//               type="password"
-//               id="password"
-//               placeholder="At least 8 characters"
-//               className="pl-[44px] pr-[12px] py-[10px] border border-black rounded-md w-full"
-//               style={{
-//                 backgroundImage: "url(/images/password-icon.svg)",
-//                 backgroundRepeat: "no-repeat",
-//                 backgroundPosition: "10px center",
-//                 backgroundSize: "16px",
-//               }}
-//             />
-//           </div>
-//           <label
-//             htmlFor="confirm-password"
-//             className="font-normal text-[12px] leading-[18px] text-darkGray mb-[4px]"
-//           >
-//             Confirm Password
-//           </label>
-//           <div className="relative mb-[24px]">
-//             <input
-//               type="password"
-//               id="confirm-password"
-//               placeholder="At least 8 characters"
-//               className="pl-[44px] pr-[12px] py-[10px] border border-black rounded-md w-full"
-//               style={{
-//                 backgroundImage: "url(/images/password-icon.svg)",
-//                 backgroundRepeat: "no-repeat",
-//                 backgroundPosition: "10px center",
-//                 backgroundSize: "16px",
-//               }}
-//             />
-//           </div>
-//           <Link
-//             href="/login"
-//             className="py-[10px] flex justify-center items-center bg-loginBtnBg text-white rounded-md mb-[24px] "
-//           >
-//             Create account
-//           </Link>
-//         </form>
-
-//         <div className="text-center md:flex md:justify-center md:items-center  ">
-//           <p className="text-grey text-[16px] font-normal leading-[24px] ">
-//             Already have an account?
-//           </p>
-//           <Link
-//             href="login"
-//             className="text-purple text-[16px] font-normal leading-[24px] "
-//           >
-//             Login
-//           </Link>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default CreateAccount;
--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import Image from "next/image";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 import React, { useState, FormEvent, ChangeEvent } from "react";
 import { auth } from "@/firebase"; // Adjust the import based on your file structure
 import { signInWithEmailAndPassword } from "firebase/auth";
@@ -9,14 +10,20 @@ function Login() {
   const [email, setEmail] = useState<string>("");
   const [password, setPassword] = useState<string>("");
   const [error, setError] = useState<string>("");
+  const router = useRouter();
 
   const handleLogin = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
     try {
-      const userCredential = await signInWithEmailAndPassword(auth, email, password);
+      const userCredential = await signInWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
       console.log(userCredential);
       // Redirect to the dashboard or home page after successful login
+      router.push("/link");
     } catch (error: any) {
       console.error(error);
       setError(error.message);
@@ -48,7 +55,10 @@ function Login() {
             Add your details below to get back into the app
           </p>
         </div>
-        <form onSubmit={handleLogin} className="flex flex-col height-auto w-full">
+        <form
+          onSubmit={handleLogin}
+          className="flex flex-col height-auto w-full"
+        >
           <label
             htmlFor="email"
             className="font-normal text-[12px] leading-[18px] text-darkGray mb-[4px]"
@@ -68,7 +78,9 @@ function Login() {
                 backgroundSize: "16px",
               }}
               value={email}
-              onChange={(e: ChangeEvent<HTMLInputElement>) => setEmail(e.target.value)}
+              onChange={(e: ChangeEvent<HTMLInputElement>) =>
+                setEmail(e.target.value)
+              }
             />
           </div>
           <label
@@ -90,7 +102,9 @@ function Login() {
                 backgroundSize: "16px",
               }}
               value={password}
-              onChange={(e: ChangeEvent<HTMLInputElement>) => setPassword(e.target.value)}
+              onChange={(e: ChangeEvent<HTMLInputElement>) =>
+                setPassword(e.target.value)
+              }
             />
           </div>
           {error && <p className="text-red-500">{error}</p>}
@@ -118,108 +132,3 @@ function Login() {
 }
 
 export default Login;
-
-
-
-
-
-
-
-
-// "use client";
-// import Image from "next/image";
-// import Link from "next/link";
-// import React from "react";
-
-// function Login() {
-//   return (
-//     <div className="p-[32px] md:bg-bgColor flex flex-col items-start md:justify-center md:items-center h-screen ">
-//       <div className="flex gap-[10px] mb-[64px] md:mb-[51px] ">
-//         <Image
-//           src="/images/Vector.svg"
-//           alt="Image vector"
-//           width="34"
-//           height="34"
-//         />
-//         <Image
-//           src="/images/devlinks.svg"
-//           alt="devlinks"
-//           width="136"
-//           height="27"
-//         />
-//       </div>
-//       <div className="w-[311px]  h-auto flex flex-col  md:bg-loginBg md:w-[476px] md:p-[40px] ">
-//         <div className="mb-[40px]">
-//           <h1 className="font-bold text-[24px] leading-[36px] mb-[8px]">
-//             Login
-//           </h1>
-//           <p className="font-normal text-[16px] leading-[24px]">
-//             Add your details below to get back into the app
-//           </p>
-//         </div>
-//         <form action="" className="flex flex-col height-auto w-full">
-//           <label
-//             htmlFor="email"
-//             className="font-normal text-[12px] leading-[18px] text-darkGray mb-[4px]"
-//           >
-//             Email address
-//           </label>
-//           <div className="relative mb-[24px]">
-//             <input
-//               type="email"
-//               id="email"
-//               placeholder="e.g. alex@email.com"
-//               className="pl-[44px] pr-[12px] py-[10px] border border-1 border-inputBorder rounded-[8px] w-full"
-//               style={{
-//                 backgroundImage: "url(/images/email-icon.svg)",
-//                 backgroundRepeat: "no-repeat",
-//                 backgroundPosition: "10px center",
-//                 backgroundSize: "16px",
-//               }}
-//             />
-//           </div>
-//           <label
-//             htmlFor="password"
-//             className="font-normal text-[12px] leading-[18px] text-darkGray mb-[4px]"
-//           >
-//             Password
-//           </label>
-//           <div className="relative mb-[24px]">
-//             <input
-//               type="password"
-//               id="password"
-//               placeholder="Enter your password"
-//               className="pl-[44px] pr-[12px] py-[10px] border border-1 border-inputBorder rounded-[8px] w-full mb-[24px] "
-//               style={{
-//                 backgroundImage: "url(/images/password-icon.svg)",
-//                 backgroundRepeat: "no-repeat",
-//                 backgroundPosition: "10px center",
-//                 backgroundSize: "16px",
-//               }}
-//             />
-//           </div>
-//           <Link
-//             href="/link"
-//             className="py-[10px] flex justify-center items-center bg-loginBtnBg text-white rounded-md mb-[24px] "
-//           >
-//             Login
-//           </Link>
-//         </form>
-
-//         <div className="text-center md:flex md:justify-center md:items-center  ">
-//           <p className="text-grey text-[16px] font-normal leading-[24px] ">
-//             Don’t have an account?
-//           </p>
-//           <Link
-//             href="/createAccount"
-//             className="text-purple text-[16px] font-normal leading-[24px] "
-//           >
-//             Create account
-//           </Link>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default Login;
